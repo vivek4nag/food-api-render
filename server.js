dd75c0c41f1a339ef23123637e68da48b20b04fa
viek4nag <vivek4nag@gmail.com>
2025-01-26 15:09:31 +0530

    test-2

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-
 const restaurants = [
   {
     id: 1,
@@ -78,7 +77,8 @@ const restaurants = [
         id: 6,
         name: "Classic Mushroom Pasta",
         price: 189,
-        imageId: "FOOD_CATALOG/IMAGES/CMS/2024/11/14/4a5b52dd-fa17-4fc9-90d4-efd0bce5fd89_ecbfdac7-d6a0-4912-99c0-7ecc147d3539.jpg",
+        imageId:
+          "FOOD_CATALOG/IMAGES/CMS/2024/11/14/4a5b52dd-fa17-4fc9-90d4-efd0bce5fd89_ecbfdac7-d6a0-4912-99c0-7ecc147d3539.jpg",
         description:
           "Serves 1 | Fusilli pasta baked in classic mushroom sauce (230 Kcal/100g). Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
         isVeg: 1,
@@ -89,7 +89,8 @@ const restaurants = [
         id: 7,
         name: "Penne McN Cheese Pasta",
         price: 189,
-        imageId: "FOOD_CATALOG/IMAGES/CMS/2024/11/14/46ffee9e-c54c-4404-b34d-52efb388b361_2f6dc8c9-f1b6-4417-a121-04700ea13ad4.jpg",
+        imageId:
+          "FOOD_CATALOG/IMAGES/CMS/2024/11/14/46ffee9e-c54c-4404-b34d-52efb388b361_2f6dc8c9-f1b6-4417-a121-04700ea13ad4.jpg",
         description:
           "Serves 1 | Penne Pasta baked in classic jalapeno sauce along with green capsicum, onion and red capsicum topped with 100% mozzarella cheese (227 Kcal/100g). Contains Cereals containing Gluten (Wheat), Soya and Milk & Milk Products.",
         isVeg: 0,
@@ -100,7 +101,8 @@ const restaurants = [
         id: 8,
         name: "Pepsi Zero Sugar 300ml",
         price: 59,
-        imageId: "FOOD_CATALOG/IMAGES/CMS/2024/3/19/b9b2a29e-bc85-48ac-b27b-0e00036fcb8f_6f1d9787-4527-4f62-a568-6975a1525344.png",
+        imageId:
+          "FOOD_CATALOG/IMAGES/CMS/2024/3/19/b9b2a29e-bc85-48ac-b27b-0e00036fcb8f_6f1d9787-4527-4f62-a568-6975a1525344.png",
         description:
           "Serves 1 | Pepsi Zero Sugar Can 300ml. Contains Caffeine (0 Kcal/100ml)",
         isVeg: 1,
@@ -111,7 +113,8 @@ const restaurants = [
         id: 9,
         name: "Sunfeast Dark Fantasy Chocolate Shake 300ml",
         price: 95,
-        imageId: "FOOD_CATALOG/IMAGES/CMS/2024/10/7/ab734e36-8258-438e-a6aa-9a6a99b25073_4611a11c-0b12-473b-99f5-e3ab23c3f22f.jpg",
+        imageId:
+          "FOOD_CATALOG/IMAGES/CMS/2024/10/7/ab734e36-8258-438e-a6aa-9a6a99b25073_4611a11c-0b12-473b-99f5-e3ab23c3f22f.jpg",
         description:
           "Treat yourself with a divine shake, made with a blend of milk and real Belgian chocolate. (109 Kcal/100 ml). Contains Milk, Nut. May Contain Soy.",
         isVeg: 1,
@@ -136,125 +139,126 @@ const restaurants = [
     id: 2,
     name: "Goila Butter Chicken",
     location: "New York",
-    cuisines: ["North Indian","Biryani","Kebabs","Desserts"],
+    cuisines: ["North Indian", "Biryani", "Kebabs", "Desserts"],
     costForTwo: "₹600 for two",
     avgRating: 4.3,
     ratingCount: "1.5K+",
-    resImageID:
-      "5e19832da032dd69547565e27104706f",
+    resImageID: "5e19832da032dd69547565e27104706f",
     deliveryTime: 29,
     menu: [
-        {
-          id: 1,
-          name: "Goila Chicken Kebab",
-          price: 379,
-          imageId: "b126bb10be6b7e157d54e5da85071fdb",
-          description:
-            "[4 pcs] Goila's version of Galouti kebab with finely ground Chicken keema seasoned with a bouquet of herbs and spices, laced with the mild smokiness of ghee, It's also known as kebabs for nawabs. Served with chutney and onion.",
-          isVeg: 0,
-          rating: "4.5",
-          ratingCount: "82 ratings",
-        },
-        {
-          id: 2,
-          name: "Dhaba Style Chili Chicken",
-          price: 359,
-          imageId: "6d09d95cdfd2687e6aafafab8851160e",
-          description:
-            "[8 pcs] Fried Chicken with some vegies tossed in our Desi inhouse sauce. Slightly tangy & spicy a mouth watering starter to start your Meal",
-          isVeg: 0,
-          rating: "3.9",
-          ratingCount: "92 ratings",
-        },
-        {
-          id: 3,
-          name: "Chicken Kalimirch",
-          price: 399,
-          imageId: "c1bb4f1012b10901fd1870cad577ff3e",
-          description:
-            "[8 pcs] Soft, juicy chicken tikka that melts in your mouth. Tender pieces of boneless chicken in our peppery malai marinade.",
-          isVeg: 0,
-          rating: "4.1",
-          ratingCount: "92 ratings",
-        },
-        {
-          id: 4,
-          name: "Goila Butter Chicken",
-          price: 589,
-          imageId: "l2uwqfqoxay2fxa4myce",
-          description:
-            "[Serves 2] MasterChef Goila's version Of The Classic Butter Chicken, Reviving The Authentic Flavours and Giving It A New Smoky Twist. Creamy and comforting",
-          isVeg: 0,
-          rating: "4.1",
-          ratingCount: "102 ratings",
-        },
-        {
-          id: 5,
-          name: "Desi Egg Curry [500ml]",
-          price: 289,
-          imageId: "FOOD_CATALOG/IMAGES/CMS/2024/4/5/d2d509b7-55d6-4733-8ab4-eee96f35cf71_b2f04679-0cc1-4707-bcf1-983eafb742cc.jpg",
-          description:
-            "[4 Full eggs] Indulge in the rich homestyle gravy and aromatic flavors of our Desi Egg Curry, a classic Indian dish simmered in a luscious blend of our in-house gravy and spices.",
-          isVeg: 0,
-          rating: "4.1",
-          ratingCount: "29 ratings",
-        },
-        {
-          id: 6,
-          name: "Chicken Tikka Masala",
-          price: 489,
-          imageId: "21b6f7adb7328a8828aa3910ff605af4",
-          description:
-            "[500 ml][Serves 2] Boneless Chicken tossed in a blend of butter gravy and onion tomato masala. It's got a bit of spice, tang and a hint of sweetness.",
-          isVeg: 0,
-          rating: "4.1",
-          ratingCount: "85 ratings",
-        },
-        {
-          id: 7,
-          name: "Party Starter Non Veg [Serves 10]",
-          price: 2299,
-          imageId: "FOOD_CATALOG/IMAGES/CMS/2024/10/25/4893a26f-18ab-41db-9dd9-12245baa269e_464e580f-1138-4ad2-a8c0-1fd4f410de59.png",
-          description:
-            "Serves 10 | Classic combination of all the starters for your party",
-          isVeg: 0,
-          rating: "4.1",
-          ratingCount: "80 ratings",
-        },
-        {
-          id: 8,
-          name: "Butter Chicken Biryani",
-          price: 459,
-          imageId: "qzq2734wagxspa67kqmv",
-          description:
-            "Goila Butter Chicken Layered With Saffron Scented Rice Topped With Fried Onions And Cashews",
-          isVeg: 0,
-          rating: "4.1",
-          ratingCount: "85 ratings",
-        },
-        {
-          id: 9,
-          name: "Butter Paneer Biryani",
-          price: 355,
-          imageId: "05def16df2855a402e5eba41ec697417",
-          description:
-            "Goila Butter Paneer Layered With Saffron Scented Rice Topped With Fried Onions And Cashews.",
-          isVeg: 1,
-          rating: "4.1",
-          ratingCount: "55 ratings",
-        },
-        {
-          id: 10,
-          name: "Gajar Ka Halwa",
-          price: 245,
-          imageId: "31ecfddcd67c9d463cc099b92f703e96",
-          description:
-            "[175 gm]\nA classic Indian dessert crafted with grated carrots, simmered in ghee, infused with cardamom, and garnished with nuts for a divine treat.",
-          isVeg: 1,
-          rating: "3.8",
-          ratingCount: "85 ratings",
-        },
-      ],
+      {
+        id: 1,
+        name: "Goila Chicken Kebab",
+        price: 379,
+        imageId: "b126bb10be6b7e157d54e5da85071fdb",
+        description:
+          "[4 pcs] Goila's version of Galouti kebab with finely ground Chicken keema seasoned with a bouquet of herbs and spices, laced with the mild smokiness of ghee, It's also known as kebabs for nawabs. Served with chutney and onion.",
+        isVeg: 0,
+        rating: "4.5",
+        ratingCount: "82 ratings",
+      },
+      {
+        id: 2,
+        name: "Dhaba Style Chili Chicken",
+        price: 359,
+        imageId: "6d09d95cdfd2687e6aafafab8851160e",
+        description:
+          "[8 pcs] Fried Chicken with some vegies tossed in our Desi inhouse sauce. Slightly tangy & spicy a mouth watering starter to start your Meal",
+        isVeg: 0,
+        rating: "3.9",
+        ratingCount: "92 ratings",
+      },
+      {
+        id: 3,
+        name: "Chicken Kalimirch",
+        price: 399,
+        imageId: "c1bb4f1012b10901fd1870cad577ff3e",
+        description:
+          "[8 pcs] Soft, juicy chicken tikka that melts in your mouth. Tender pieces of boneless chicken in our peppery malai marinade.",
+        isVeg: 0,
+        rating: "4.1",
+        ratingCount: "92 ratings",
+      },
+      {
+        id: 4,
+        name: "Goila Butter Chicken",
+        price: 589,
+        imageId: "l2uwqfqoxay2fxa4myce",
+        description:
+          "[Serves 2] MasterChef Goila's version Of The Classic Butter Chicken, Reviving The Authentic Flavours and Giving It A New Smoky Twist. Creamy and comforting",
+        isVeg: 0,
+        rating: "4.1",
+        ratingCount: "102 ratings",
+      },
+      {
+        id: 5,
+        name: "Desi Egg Curry [500ml]",
+        price: 289,
+        imageId:
+          "FOOD_CATALOG/IMAGES/CMS/2024/4/5/d2d509b7-55d6-4733-8ab4-eee96f35cf71_b2f04679-0cc1-4707-bcf1-983eafb742cc.jpg",
+        description:
+          "[4 Full eggs] Indulge in the rich homestyle gravy and aromatic flavors of our Desi Egg Curry, a classic Indian dish simmered in a luscious blend of our in-house gravy and spices.",
+        isVeg: 0,
+        rating: "4.1",
+        ratingCount: "29 ratings",
+      },
+      {
+        id: 6,
+        name: "Chicken Tikka Masala",
+        price: 489,
+        imageId: "21b6f7adb7328a8828aa3910ff605af4",
+        description:
+          "[500 ml][Serves 2] Boneless Chicken tossed in a blend of butter gravy and onion tomato masala. It's got a bit of spice, tang and a hint of sweetness.",
+        isVeg: 0,
+        rating: "4.1",
+        ratingCount: "85 ratings",
+      },
+      {
+        id: 7,
+        name: "Party Starter Non Veg [Serves 10]",
+        price: 2299,
+        imageId:
+          "FOOD_CATALOG/IMAGES/CMS/2024/10/25/4893a26f-18ab-41db-9dd9-12245baa269e_464e580f-1138-4ad2-a8c0-1fd4f410de59.png",
+        description:
+          "Serves 10 | Classic combination of all the starters for your party",
+        isVeg: 0,
+        rating: "4.1",
+        ratingCount: "80 ratings",
+      },
+      {
+        id: 8,
+        name: "Butter Chicken Biryani",
+        price: 459,
+        imageId: "qzq2734wagxspa67kqmv",
+        description:
+          "Goila Butter Chicken Layered With Saffron Scented Rice Topped With Fried Onions And Cashews",
+        isVeg: 0,
+        rating: "4.1",
+        ratingCount: "85 ratings",
+      },
+      {
+        id: 9,
+        name: "Butter Paneer Biryani",
+        price: 355,
+        imageId: "05def16df2855a402e5eba41ec697417",
+        description:
+          "Goila Butter Paneer Layered With Saffron Scented Rice Topped With Fried Onions And Cashews.",
+        isVeg: 1,
+        rating: "4.1",
+        ratingCount: "55 ratings",
+      },
+      {
+        id: 10,
+        name: "Gajar Ka Halwa",
+        price: 245,
+        imageId: "31ecfddcd67c9d463cc099b92f703e96",
+        description:
+          "[175 gm]\nA classic Indian dessert crafted with grated carrots, simmered in ghee, infused with cardamom, and garnished with nuts for a divine treat.",
+        isVeg: 1,
+        rating: "3.8",
+        ratingCount: "85 ratings",
+      },
+    ],
   },
 
   {
@@ -273,17 +277,32 @@ const restaurants = [
   },
 ];
 
-
 app.get("/restaurants", (req, res) => {
-  const restaurantList = restaurants.map(({ id, name, location, cuisine }) => ({
-    id,
-    name,
-    location,
-    cuisine,
-  }));
+  const restaurantList = restaurants.map(
+    ({
+      id,
+      name,
+      location,
+      cuisine,
+      costForTwo,
+      avgRating,
+      ratingCount,
+      resImageID,
+      deliveryTime,
+    }) => ({
+      id,
+      name,
+      location,
+      cuisine,
+      costForTwo,
+      avgRating,
+      ratingCount,
+      resImageID,
+      deliveryTime,
+    })
+  );
   res.json(restaurantList);
 });
-
 
 app.get("/restaurants/:id/menu", (req, res) => {
   const restaurantId = parseInt(req.params.id);
@@ -296,6 +315,7 @@ app.get("/restaurants/:id/menu", (req, res) => {
   res.json(restaurant.menu);
 });
 
-
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
